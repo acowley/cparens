c14e0454ba04cbdb620c203e9cd3a31c9b9ff54b
Anthony Cowley <acowley@gmail.com>
2019-09-09 10:55:11 -0400

    Change upper-bound on base to 5

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, language-c, pretty, stdenv }:
 mkDerivation {
   pname = "cparens";
-  version = "0.1.0.0";
+  version = "0.1.0.1";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
